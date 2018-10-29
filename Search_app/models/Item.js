b6b5ee1a709ef4acc5462f97efcdf72c031c8c9e
Jesper Brynildsen <jesper_bry@hotmail.com>
2018-10-29 19:11:07 +0100

    Add schema for beers #1

--- Search_app/models/Item.js
+++ Search_app/models/Item.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
+const Float = require('mongoose-float').loadType(mongoose);
 
 const Schema = mongoose.Schema;
 
 // Creates Schema:
 const itemSchema = new Schema({
-  // Schema goes her..
-  // name: {
-  //      type: String
-  // },
-  // phone: {
-  //   type: String
-  // }
-
+  abv: { type: Float },
+  ibu: { type: Schema.Types.Mixed },
+  id: { type: Number },
+  name: { type: String },
+  style: { type: String },
+  brewery_id: { type: Number },
+  ounces: { type: Float }
+  
   // https://mongoosejs.com/docs/guide.html
-});
+}, { collection: 'beers' });
 
 module.exports = Item = mongoose.model('item', itemSchema);
