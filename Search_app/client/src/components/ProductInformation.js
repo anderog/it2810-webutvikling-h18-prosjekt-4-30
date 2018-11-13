d159a0c72c38d16c3207a6c241bb2e4b2e034c91
Jesper Brynildsen <jesper_bry@hotmail.com>
2018-11-13 12:40:46 +0100

    Style for productinformation #5 #14

--- Search_app/client/src/components/ProductInformation.js
+++ Search_app/client/src/components/ProductInformation.js
@@ -5,34 +5,37 @@ export default class ProductInformation extends Component {
   render() {
     const product = this.props.productItem;
     return (
-      <div className={"infoContainer"}>
-        <div className={"row1"}>
-          <h4 className={"infoHeader"}>Info</h4>
-          <p>Volum: {product.Volum}</p>
-          <p>Alkohol: {product.Alkohol}%</p>
-          <p>Land: {product.Land}</p>
-        </div>
-        <div className={"row2"}>
-          <h4 className={"infoHeader"}>Pris</h4>
-          <p>
-            Pris: {product.Pris}
-            ,-
-          </p>
-          <p>
-            Literpris: {product.Literpris}
-            ,-
-          </p>
-          <p>
-            Pris per alkoholenhet:{" "}
-            {(product.Literpris / (product.Alkohol / 100) / (200 / 3)).toFixed(
-              2
-            )}
-            {""}
-            ,-
-          </p>
-        </div>
-        <div className={"row3"}>
-          <h4 className={"infoHeader"}>Smak</h4>
+      <div className="paragraph">
+        <p>
+          <strong>Smak:</strong> {product.Smak}
+        </p>
+        <p>
+          <strong>Lukt:</strong> {product.Lukt}
+        </p>
+        <p>
+          <strong>Farge:</strong> {product.Farge}
+        </p>
+        <div className={"infoContainer"}>
+          <div className={"row1"}>
+            <h4 className={"infoHeader"}>Info</h4>
+            <p>Volum: {product.Volum} liter</p>
+            <p>Alkohol: {product.Alkohol} %</p>
+            <p>Land: {product.Land}</p>
+          </div>
+          <div className={"row2"}>
+            <h4 className={"infoHeader"}>Pris</h4>
+            <p>Pris: {product.Pris} ,-</p>
+            <p>Literpris: {product.Literpris} ,-</p>
+            <p>
+              Pris per alkoholenhet:{" "}
+              {(
+                product.Literpris /
+                (product.Alkohol / 100) /
+                (200 / 3)
+              ).toFixed(2)}
+              {""} ,-
+            </p>
+          </div>
         </div>
       </div>
     );
