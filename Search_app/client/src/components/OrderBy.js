cb2f3137f21c686e9c0dcbab4c04d4ff49ff8e2d
olekfur <olekfur@gmail.com>
2018-11-13 12:17:40 +0100

    Add sorting by name

--- Search_app/client/src/components/OrderBy.js
+++ Search_app/client/src/components/OrderBy.js
@@ -6,17 +6,44 @@ class OrderBy extends React.Component {
   constructor(props) {
     super(props);
     this.togglePriceSort = this.togglePriceSort.bind(this);
-    this.state = { orderByPrice: false };
+    this.state = {
+      orderByPrice: false,
+      orderByName: true
+    };
   }
 
   togglePriceSort = () => {
     if (this.state.orderByPrice) {
-      this.setState({ orderByPrice: !this.state.orderByPrice });
+      this.setState({
+        orderByPrice: false,
+        orderByName: false
+      });
       this.props.dispatch({ type: "PRICE_ASC" });
       this.props.dispatch(fetchProducts());
     } else {
       this.props.dispatch({ type: "PRICE_DESC" });
-      this.setState({ orderByPrice: !this.state.orderByPrice });
+      this.setState({
+        orderByPrice: true,
+        orderByName: false
+      });
+      this.props.dispatch(fetchProducts());
+    }
+  };
+
+  toggleNameSort = () => {
+    if (!this.state.orderByName) {
+      this.setState({
+        orderByPrice: false,
+        orderByName: true
+      });
+      this.props.dispatch({ type: "NAME_ASC" });
+      this.props.dispatch(fetchProducts());
+    } else {
+      this.setState({
+        orderByPrice: false,
+        orderByName: false
+      });
+      this.props.dispatch({ type: "NAME_DESC" });
       this.props.dispatch(fetchProducts());
     }
   };
@@ -25,6 +52,7 @@ class OrderBy extends React.Component {
     return (
       <div className="wrapper">
         <button onClick={this.togglePriceSort}>Pris</button>
+        <button onClick={this.toggleNameSort}>Alfabetisk</button>
       </div>
     );
   }
